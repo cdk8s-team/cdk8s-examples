e2ebafdd81da8b849630d608c6b415ba40deb802
epolon <epolon@amazon.com>
2023-10-05 17:34:04 +0300

    mid work
    Signed-off-by: epolon <epolon@amazon.com>

---
+++ .projenrc.ts
@@ -0,0 +1,135 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { Cdk8sTeamTypeScriptProject } from '@cdk8s/projen-common';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { globSync } from 'glob';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { renderString } from 'nunjucks';
+import { TextFile, YamlFile } from 'projen';
+
+type Lang = 'go' | 'python' | 'java';
+
+const project = new Cdk8sTeamTypeScriptProject({
+  defaultReleaseBranch: 'main',
+  name: 'root',
+  devDeps: [
+    '@cdk8s/projen-common',
+    '@aws-cdk/lambda-layer-kubectl-v25',
+    '@types/nunjucks',
+    'lerna',
+    'cdk8s',
+    'constructs',
+    'cdk8s-plus-26',
+    'cdk8s-cli',
+    'aws-cdk-lib',
+    'aws-cdk',
+    'nunjucks',
+    'glob',
+  ],
+  sampleCode: false,
+  projenrcTs: true,
+  release: false,
+  jest: false,
+  workflowBootstrapSteps: [
+    {
+      uses: 'actions/setup-python@v4',
+      with: {
+        'python-version': '3.10.4',
+      },
+    },
+    {
+      name: 'Install pipenv',
+      run: 'pip install pipenv',
+    },
+    {
+      uses: 'actions/setup-java@v3',
+      with: {
+        'distribution': 'adopt',
+        'java-version': '19.0.2',
+      },
+    },
+    {
+      name: 'Install Maven',
+      uses: 'stCarolas/setup-maven@v4.5',
+      with: {
+        'maven-version': '3.9.1',
+      },
+    },
+  ],
+});
+
+project.package.addField('private', true);
+project.package.addField('workspaces', {
+  packages: [
+    'typescript/**/*',
+    'go/**/*',
+    'python/**/*',
+    'java/**/*',
+  ],
+});
+
+project.testTask.reset();
+
+(project.buildTask as any)._locked = false;
+project.buildTask.reset('lerna run build --skip-nx-cache --no-bail');
+(project.buildTask as any)._locked = true;
+
+// no package task is needed
+project.packageTask.reset();
+
+project.gitignore.exclude('.vscode');
+
+project.tasks.removeTask('test');
+project.tasks.removeTask('test:update');
+// next version requires node >= 18
+project.package.addField('resolutions', {
+  '@yarnpkg/parsers': '3.0.0-rc.48.1',
+});
+
+renderManifests();
+
+project.synth();
+
+function manifestTemplate(lang: Lang) {
+  switch (lang) {
+    case 'go':
+      return 'go.mod';
+    case 'java':
+      return 'pom.xml';
+    case 'python':
+      return 'Pipfile';
+  }
+}
+
+function findManifestTemplates(lang: Lang) {
+
+  return globSync(`**/${manifestTemplate(lang)}`, { cwd: path.join(__dirname, lang), nodir: true })
+    .map(f => path.join(__dirname, lang, f));
+}
+
+function renderManifests() {
+
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  const manifest = require('./package.json');
+
+  const context = {
+    cdk8sVersion: manifest.devDependencies.cdk8s.slice(1),
+    cdk8sPlusVersion: manifest.devDependencies['cdk8s-plus-26'].slice(1),
+    awsCdkLibVersion: manifest.devDependencies['aws-cdk-lib'].slice(1),
+  };
+
+  const files: string[] = [
+    ...findManifestTemplates('go'),
+    ...findManifestTemplates('java'),
+    ...findManifestTemplates('python'),
+  ];
+
+  for (const f of files) {
+    const current = fs.readFileSync(`${f}.template`, { encoding: 'utf-8' });
+    const rendered = renderString(current, context );
+    new TextFile(project, '', {
+      lines: rendered.split('\n'),
+    });
+  }
+
+}
