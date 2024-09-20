a175a174952731cfc59febe0ed61ced96fe1584d
cdk8s-automation <81352262+cdk8s-automation@users.noreply.github.com>
2024-09-20 18:19:09 +0300

    chore(deps): upgrade configuration (#469)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdk8s-team/cdk8s-examples/actions/runs/10961698543
    
    ------
    
    *Automatically created by projen via the "upgrade-configuration" workflow*

--- go/cdk8s-crd/imports/certmanagerio/CertificateSpecPrivateKeyRotationPolicy.go
+++ go/cdk8s-crd/imports/certmanagerio/CertificateSpecPrivateKeyRotationPolicy.go
@@ -4,7 +4,7 @@ package certmanagerio
 // RotationPolicy controls how private keys should be regenerated when a re-issuance is being processed.
 //
 // If set to `Never`, a private key will only be generated if one does not
-// already exist in the target `spec.secretName`. If one does exists but it
+// already exist in the target `spec.secretName`. If one does exist but it
 // does not have the correct algorithm or size, a warning will be raised
 // to await user intervention.
 // If set to `Always`, a private key matching the specified requirements
--- python/cdk8s-crd/imports/io/cert_manager/__init__.py
+++ python/cdk8s-crd/imports/io/cert_manager/__init__.py
@@ -1541,7 +1541,7 @@ class CertificateSpecPrivateKey:
 
         :param algorithm: Algorithm is the private key algorithm of the corresponding private key for this certificate. If provided, allowed values are either ``RSA``, ``ECDSA`` or ``Ed25519``. If ``algorithm`` is specified and ``size`` is not provided, key size of 2048 will be used for ``RSA`` key algorithm and key size of 256 will be used for ``ECDSA`` key algorithm. key size is ignored when using the ``Ed25519`` key algorithm.
         :param encoding: The private key cryptography standards (PKCS) encoding for this certificate's private key to be encoded in. If provided, allowed values are ``PKCS1`` and ``PKCS8`` standing for PKCS#1 and PKCS#8, respectively. Defaults to ``PKCS1`` if not specified. Default: PKCS1` if not specified.
-        :param rotation_policy: RotationPolicy controls how private keys should be regenerated when a re-issuance is being processed. If set to ``Never``, a private key will only be generated if one does not already exist in the target ``spec.secretName``. If one does exists but it does not have the correct algorithm or size, a warning will be raised to await user intervention. If set to ``Always``, a private key matching the specified requirements will be generated whenever a re-issuance occurs. Default is ``Never`` for backward compatibility. Default: Never` for backward compatibility.
+        :param rotation_policy: RotationPolicy controls how private keys should be regenerated when a re-issuance is being processed. If set to ``Never``, a private key will only be generated if one does not already exist in the target ``spec.secretName``. If one does exist but it does not have the correct algorithm or size, a warning will be raised to await user intervention. If set to ``Always``, a private key matching the specified requirements will be generated whenever a re-issuance occurs. Default is ``Never`` for backward compatibility. Default: Never` for backward compatibility.
         :param size: Size is the key bit size of the corresponding private key for this certificate. If ``algorithm`` is set to ``RSA``, valid values are ``2048``, ``4096`` or ``8192``, and will default to ``2048`` if not specified. If ``algorithm`` is set to ``ECDSA``, valid values are ``256``, ``384`` or ``521``, and will default to ``256`` if not specified. If ``algorithm`` is set to ``Ed25519``, Size is ignored. No other values are allowed.
 
         :schema: CertificateSpecPrivateKey
@@ -1599,7 +1599,7 @@ class CertificateSpecPrivateKey:
         '''RotationPolicy controls how private keys should be regenerated when a re-issuance is being processed.
 
         If set to ``Never``, a private key will only be generated if one does not
-        already exist in the target ``spec.secretName``. If one does exists but it
+        already exist in the target ``spec.secretName``. If one does exist but it
         does not have the correct algorithm or size, a warning will be raised
         to await user intervention.
         If set to ``Always``, a private key matching the specified requirements
@@ -1686,7 +1686,7 @@ class CertificateSpecPrivateKeyRotationPolicy(enum.Enum):
     '''RotationPolicy controls how private keys should be regenerated when a re-issuance is being processed.
 
     If set to ``Never``, a private key will only be generated if one does not
-    already exist in the target ``spec.secretName``. If one does exists but it
+    already exist in the target ``spec.secretName``. If one does exist but it
     does not have the correct algorithm or size, a warning will be raised
     to await user intervention.
     If set to ``Always``, a private key matching the specified requirements
